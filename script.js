1ac501d1bc44cb432d1208e24ed183c71b919d9b
ajPCS <ajmcguire@pcs-nyc.org>
2024-02-02 15:17:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let manateeHeader = document.getElementById("manatee-header");
+manateeHeader.addEventListener("click", changeBackground);
+
+
+function changeBackground() {
+
+    document.getElementById("body-background").style.backgroundColor = "black";
+    console.log("wuuuush")
+}
